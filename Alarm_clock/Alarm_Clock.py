8ab762b17d0dedf9737daef2dbceab00bd16e9e2
ZEYAD MAHMOUD AMIN <zyadnda02@gmail.com>
2025-08-07 04:39:55 +0200

    Add files via upload

---
+++ Alarm_clock/Alarm_Clock.py
@@ -0,0 +1,31 @@
+import pygame
+import time
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")
+    pygame.mixer.init()
+    pygame.mixer.music.load("alarm.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+
+
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+total_seconds = (minutes * 60) + seconds
+alarm(total_seconds)
