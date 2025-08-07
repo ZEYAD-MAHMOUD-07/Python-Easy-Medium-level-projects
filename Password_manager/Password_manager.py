b9d37bf29ed6df2c9312398f5bc3f5d58cfae4e0
ZEYAD MAHMOUD AMIN <zyadnda02@gmail.com>
2025-08-07 05:16:59 +0200

    Update Password_manager.py

--- Password_manager/Password_manager.py
+++ Password_manager/Password_manager.py
@@ -15,7 +15,6 @@ def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
-هذه الدالة تُستخدم مرة واحدة فقط لإنشاء مفتاح التشفير وتخزينه في ملف اسمه
 key.key
 """
 
@@ -31,17 +30,7 @@ def load_key():
 # i can ignore this line
 key = load_key()  # + master_pwd.encode()
 fer = Fernet(key)
-"""
-input(...): يطلب من المستخدم إدخال كلمة مرور رئيسية.
 
-load_key(): يحصل على المفتاح من الملف.
-
-master_pwd.encode(): يحوّل كلمة المرور من string إلى bytes لأن Fernet يتعامل مع bytes.
-
-fer = Fernet(key):
-
-ينشئ كائن مسؤول عن التشفير وفك التشفير باستخدام المفتاح.
-"""
 
 
 def view():
@@ -52,27 +41,6 @@ def view():
             print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 
-"""
-with open("password.txt", "r") as r: يفتح الملف الذي فيه الحسابات وكلمات المرور المشفرة.
-
-for line in r.readlines(): يقرأ كل سطر في الملف.
-
-line.rstrip(): يزيل الفراغات أو \n في نهاية السطر.
-
-user, passw = data.split(" | "): يفصل اسم الحساب وكلمة المرور المشفرة.
-
-fer.decrypt(passw.encode()): يفك تشفير كلمة المرور.
-
-لماذا نستخدم الاثنين معًا؟
-لأننا:
-
-نحول إلى bytes عندما نشفر أو نفك التشفير (encode()).
-
-نحول إلى string عندما نكتب إلى ملف أو نعرض للمستخدم (decode()).
-
-هما مش ضد بعض، بل مكملين لبعض علشان نتعامل مع المكتبة ونخزن البيانات بشكل سليم.
-
-"""
 
 
 def add():
@@ -83,15 +51,6 @@ def add():
         f.write(name + " | " + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
-"""
-pwd.encode(): تحويل كلمة المرور إلى bytes.
-
-fer.encrypt(...): تشفير كلمة المرور.
-
-.decode(): تحويل النتيجة إلى string حتى نقدر نخزنها في ملف نصي.
-
-f.write(...): يضيف السطر إلى الملف بصيغة:
-"""
 
 
 while True:
@@ -111,3 +70,4 @@ while True:
     else:
         print("Invalid mode.")
         continue
+
